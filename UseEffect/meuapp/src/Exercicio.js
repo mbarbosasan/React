e2a8e23441f730d646abba0f1df55e64550fbd1d
moisesbarbosa23@gmail.com <moisesbarbosa23@gmail.com>
2022-03-09 19:25:45 -0300

    Concluído módulo de UseEffect

---
+++ UseEffect/meuapp/src/Exercicio.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Produto from "./Produto";
+
+// Quando o usuário clicar em um dos botões, faça um fetch do produto clicado utilizando a api abaixo
+// https://ranekapi.origamid.dev/json/api/produto/notebook
+// https://ranekapi.origamid.dev/json/api/produto/smartphone
+// Mostre o nome e preço na tela (separe essa informação em um componente Produto.js)
+// Defina o produto clicado como uma preferência do usuário no localStorage
+// Quando o usuário entrar no site, se existe um produto no localStorage, faça o fetch do mesmo
+
+const Exercicio = () => {
+  const [produto, setProduto] = React.useState(null);
+
+  React.useEffect(() => {
+    const produtoLocal = window.localStorage.getItem('produto');
+    if (produtoLocal !== null) setProduto(produtoLocal);
+    console.log(produtoLocal);
+  }, []);
+
+  React.useEffect(() => {
+    if (produto !== null) {
+      window.localStorage.setItem('produto', produto);
+    }
+  }, [produto])
+  
+
+  function handleClick({target}) {
+    setProduto(target.innerText);
+  }
+
+  return (
+    <div>
+      <h1>Preferência: {produto}</h1>
+      <button onClick={handleClick}>Notebook</button>
+      <button onClick={handleClick}>Smartphone</button>
+      <Produto produto={produto}/>
+    </div>
+  );
+};
+
+export default Exercicio;
